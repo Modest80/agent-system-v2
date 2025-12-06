1acc66b4a95934986a6f2d2fc2f35d74d2319aa6
modest80 <modesting@gmail.com>
2025-12-06 14:16:03 +0400

    Набор полезных SQL запросов

---
+++ sql/utils.sql
@@ -0,0 +1,6 @@
+-- Удобное удаление таблиц в процессе отладки приложения
+DROP TABLE IF EXISTS chunks CASCADE;
+DROP TABLE IF EXISTS document_categories CASCADE;
+DROP TABLE IF EXISTS documents CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
